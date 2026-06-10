5927ee9745fb0097c3992be89122f07dbf69a33b
Jimmy Green <jimmy@green.com>
2026-06-10 18:01:01 +0100

    feat: Add Electron wrapper support for standalone desktop app build

---
+++ main.cjs
@@ -0,0 +1,63 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  const isDev = !app.isPackaged;
+
+  mainWindow = new BrowserWindow({
+    width: 1240,
+    height: 860,
+    minWidth: 1000,
+    minHeight: 700,
+    backgroundColor: '#000000',
+    title: 'Delta7 Workstation Console',
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      sandbox: true,
+    },
+  });
+
+  mainWindow.setMenuBarVisibility(false);
+
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:5173');
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, 'dist', 'index.html'));
+  }
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+const additionalData = { myKey: 'delta7synth-instance' };
+const gotTheLock = app.requestSingleInstanceLock(additionalData);
+
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory, additionalData) => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.focus();
+    }
+  });
+
+  app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+  });
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
